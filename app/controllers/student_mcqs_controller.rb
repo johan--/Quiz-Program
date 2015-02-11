9e5d1e95f8b9cf3ec752b9d2c67a06a87704f979
raymon1 <raymonmina@gmail.com>
2015-02-12 01:03:16 +0200

    Update student_mcqs_controller.rb

--- app/controllers/student_mcqs_controller.rb
+++ app/controllers/student_mcqs_controller.rb
@@ -26,15 +26,19 @@ class StudentMcqsController < ApplicationController
   # BOSSSS HENAAAAAAAAAAAAAAAAAAA!!!!!!!!!!!!!!!!!!!!!!!!!
   def create
     quiz_id1 = current_student_most_recent_quiz.id #m3rfsh most recent quiz di tetgab ezay 
-    mcq_id1 = Mcq.where(quiz_id: quiz_id1).id
+    #mcq_id1 = Mcq.where(quiz_id: quiz_id1).id
+    mcqs1 = Mcq.where(quiz_id: quiz_id1)
 
     params[:answers].each_index do |i|   
-      McqAnswer.create(student_answer: params[:answers][i],
-                       mcq_id: mcq_id1[i],
+      @stu_mcq = StudentMcq.create(student_answer: params[:answers][i],
+                       mcq_id: mcqs1[i].id,
                        student_id: current_student.id)
+      if @stu_mcq[:student_answer] == mcqs1[i][:answer]
+        grade = grade + mcqs1[i][:question_mark]
+      end
     end
 
-     redirect_to controller: "StudentQuizzesController", action: "create", quiz_id1
+     redirect_to controller: "StudentQuizzesController", action: "create", q{quiz_id: quiz_id1, g: grade}
   end
 
   # PATCH/PUT /student_mcqs/1
