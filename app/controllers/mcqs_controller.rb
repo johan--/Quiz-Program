d6229f83dc070fd1309dd7949954e818da57aa58
raymon1 <raymonmina@gmail.com>
2015-02-12 01:08:48 +0200

    Update mcqs_controller.rb

--- app/controllers/mcqs_controller.rb
+++ app/controllers/mcqs_controller.rb
@@ -5,10 +5,10 @@ class McqsController < ApplicationController\
 		params[mcq:].each do |mcq1|
    			Mcq.create(question_mark: mcq1[:question_mark], 
    				       answer: mcq1[:answer],
-   				       choices0:  mcq1[:choices][0],
-   				       choices1:  mcq1[:choices][1],
-   				       choices2:  mcq1[:choices][2],
-   				       choices3:  mcq1[:choices][3],
+   				       choices1:  mcq1[:choices][0],
+   				       choices2:  mcq1[:choices][1],
+   				       choices3:  mcq1[:choices][2],
+   				       choices4:  mcq1[:choices][3],
    				       quiz_id: quiz1.id,
    				       question_statement: mcq1[:question_statement])
          end
